496cac2ce9a09e518f25f06ba8a873b4a9cd9ce7
valeriisurzhenko <valera_dzr@mail.ru>
2022-10-25 22:53:59 +0200

    final

--- part1/practice/bad_smell_big_class/main.py
+++ part1/practice/bad_smell_big_class/main.py
@@ -10,13 +10,34 @@
 # Ловушка не может ничего кроме как атаковать того, кто на нее наступит
 # Для решения этой задачи не используйте наследование
 
-class Obj:
-    ##
-    # тут представлено поведение четырех различных игровых объектов:
-    # - воина
-    # - лекаря
-    # - дерева
-    # - ловушки
+class Trap:
+
+    def attack(self):
+        pass
+
+
+class Tree:
+
+    def defense(self):
+        pass
+
+    def on_fire(self):
+        pass
+
+
+class Healer:
+
+    def defense(self):
+        pass
+
+    def heal(self):
+        pass
+
+    def move(self):
+        pass
+
+
+class Warrior:
 
     def attack(self):
         pass
@@ -27,23 +48,6 @@ class Obj:
     def move(self):
         pass
 
-    def healer_defense(self):
-        pass
-
-    def healer_move(self):
-        pass
-
-    def heal(self):
-        pass
-
-    def tree_defense(self):
-        pass
-
-    def on_fire(self):
-        pass
-
-    def trap_attack(self):
-        print("It's a trap!")
 
 if __name__ == '__main__':
     unit = Warrior()
--- part1/practice/bad_smell_long_parameter_list/main.py
+++ part1/practice/bad_smell_long_parameter_list/main.py
@@ -9,6 +9,34 @@
 # В этом примере есть сразу несколько запахов плохого кода. Исправьте их
 #   (длинный метод, длинный список параметров)
 
+class Unit:
+    def __init__(self, status, field, x, y, speed):
+        self.status = status
+        self.field = field
+        self.x = x
+        self.y = y
+        self.speed = speed
+
+    def move(self, direction):
+        speed = self._get_speed
+
+        if direction == "UP":
+            self.field.set_unit(y=self.y + speed, x=self.x, unit=self)
+        elif direction == "DOWN":
+            self.field.set_unit(y=self.y - speed, x=self.x, unit=self)
+        elif direction == "LEFT":
+            self.field.set_unit(y=self.y, x=self.x - speed, unit=self)
+        elif direction == "RIGHT":
+            self.field.set_unit(y=self.y, x=self.x + speed, unit=self)
+
+    def _get_speed(self):
+        if self.status == 'fly':
+            return self.speed * 1.2
+        elif self.status == 'crawl':
+            return self.speed * 0.5
+        else:
+            raise ValueError("Эк тебя разкорячило")
+
 
 class Unit:
     def move(self, field, x_coord, y_coord, direction, is_fly, crawl, speed = 1):
